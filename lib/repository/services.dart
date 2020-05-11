96850b6de521de730a94e567a5e6391690e21250
Ekpoto Liberty Bernard <ekpoto.liberty@gmail.com>
2020-05-11 20:26:14 +0100

    remove syntax error

--- lib/repository/services.dart
+++ lib/repository/services.dart
@@ -10,8 +10,7 @@ Future fetchAllTask() async {
     List collections = json.decode(response.body);
     return collections;
 //    return collections.map((model) => Task.fromJson(model)).toList();
-  } else git add .
-
+  } else {
     // If the server did not return a 200 OK response, then throw an exception.
     throw Exception('Failed to load notifications from API');
   }
